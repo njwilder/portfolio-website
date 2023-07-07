8bf1a3aa41bcad883351d6d915122a659d85877f
njwilder <nwilder506@gmail.com>
2023-07-07 12:32:00 -0400

    Practicing with JS, added paralax and about div, along with UL of different potential page sections.

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function say_hello(name="Person") {
+    // console.log("Hello " + name + "!");
+    return "Hello " + name + "!";
+}
+
+say_hello();
+say_hello("Noah");
+
+// window.alert("HELLO. THIS IS NOT A DRILL. I REPEAT, THIS IS NOT A DRILL!");
+// document.getElementById("header").innerHTML = say_hello("Jake");
