a282e3e6d13a70290c0e4e77e82ca895e893dc55
njwilder <nwilder506@gmail.com>
2024-02-23 09:10:40 -0500

    Made JS a little more generic, then solved an edgecase related to css @media font scaling and projectItem header calculation.

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ const colorLight1 = "#89a257";
 const colorLight2 = "#bedc7f";
 const colorLight3 = "#eeffcc";
 
+const hamburgerId = "hamburger";
+const projectItemClassName = "projectItem";
+const projectDescriptionButtonId = "projectDescriptionButton";
+
 
 
 /**
  * Open and close the sidebar menu for navigation when the hamburger is toggled.
- * @param {string} hamburgerID the hamburger button element id
  */
-function toggleHamburger (hamburgerID) {
-    let hamburger = document.getElementById(hamburgerID);
+function toggleHamburger () {
+    let hamburger = document.getElementById(hamburgerId);
     let sidebar = document.getElementById("sidebar");
     let navbarList = sidebar.children[1].children[0];
     if (hamburger.value == "closed") {
@@ -49,39 +52,73 @@ function toggleHamburger (hamburgerID) {
 }
 
 
-// function toggleDescription() {
+/**
+ * Helper function for toggleDescriptions, broken out functionality for other functions to use
+ * @param {string} startValue "enabled" or "disabled" to know how to manipulate the project description
+ * @param {*} projectItem projectItem element to be manipulated  
+ */
+function toggleDescription(startValue, projectItem) {
+    let header = projectItem.children[0]
+    let description = projectItem.children[2];
+    let descriptionStyle = description.style;
+    
+    if (startValue == "disabled") {
+        descriptionStyle.position = "relative";
+        descriptionStyle.opacity  = "1";
+        descriptionStyle.height   = "fit-content";
 
-// }
-
-function toggleDescriptions(button) {
-    let projectItems = document.getElementsByClassName("projectItem");
-    if (button.value == "disabled") {
-        for (let item = 0; item < projectItems.length; item++) {
-            let headerStyle = projectItems[item].children[0].style;
-            let description = projectItems[item].children[2];
-            let descriptionStyle = description.style;
-            descriptionStyle.position = "relative";
-            descriptionStyle.opacity  = "1";
-            descriptionStyle.height   = "fit-content";
-            headerStyle.bottom = description.offsetHeight.toString() + "px";
-        }
-        button.value = "enabled"
-
-    // Disable descriptions
+        header.style.top = (projectItem.children[1].clientHeight - header.clientHeight).toString() + "px";
+        header.style.bottom = "";
     } else {
-        for (let item = 0; item < projectItems.length; item++) {
-            let headerStyle = projectItems[item].children[0].style;
-            let description = projectItems[item].children[2];
-            let descriptionStyle = description.style;
-            descriptionStyle.position = "absolute";
-            descriptionStyle.opacity  = ""; // If this is set to '0', it will cease to transition on hover
-            descriptionStyle.height   = "100%";
-            headerStyle.bottom = "0";
-        }
+        descriptionStyle.position = "absolute";
+        descriptionStyle.opacity  = "";
+        descriptionStyle.height   = "100%";
+
+        header.style.top = "";
+        header.style.bottom = "0";
+    }
+}
+
+function toggleDescriptions() {
+    let button = document.getElementById(projectDescriptionButtonId);
+    let projectItems = document.getElementsByClassName(projectItemClassName);
+    for (let item = 0; item < projectItems.length; item++) {
+        toggleDescription(button.value, projectItems[item]);
+    }
+    if (button.value == "enabled") {
         button.value = "disabled";
+    } else {
+        button.value = "enabled";
+    }
+}
+
+function updateProjItemHeaderHeight() {
+    let buttonVal = document.getElementById("projectDescriptionButton").value;
+    // Reset the header size
+    if (buttonVal == "enabled") {
+        console.log("Firing off update event");
+        let projectItems = document.getElementsByClassName(projectItemClassName);
+        for (let item = 0; item < projectItems.length; item++) {
+            // Redo the toggleDescription calculations to ensure proper text heights
+            toggleDescription("enabled", projectItems[item]);
+            toggleDescription("disabled", projectItems[item]);
+        }
     }
 }
 
 
+const fontScalarMin = window.matchMedia("(min-width: 700px)");
+const fontScalarMax = window.matchMedia("(max-width: 700px)");
 
-
+// Whenever the 700px threshold is passed, we need to recalculate header heights for potentially
+// enabled descriptions.
+fontScalarMin.addEventListener("change", e => {
+    if (e.matches) {
+        updateProjItemHeaderHeight();
+    }
+})
+fontScalarMax.addEventListener("change", e => {
+    if (e.matches) {
+        updateProjItemHeaderHeight();
+    }
+})
